659617822df4f67d21c4ac33d9cf2f516361d21b
Nicolas Pochet <npochet@gmail.com>
2018-08-22 21:05:55 +0200

    Create first route

---
+++ README.md
@@ -0,0 +1,1 @@
+# Demo Application using Gotham
---
+++ src/main.rs
@@ -0,0 +1,33 @@
+extern crate gotham;
+extern crate hyper;
+extern crate mime;
+
+use gotham::http::response::create_response;
+use gotham::router::builder::*;
+use gotham::router::Router;
+use gotham::state::State;
+use hyper::{Response, StatusCode};
+
+fn get_appointments(state: State) -> (State, Response) {
+    let res = create_response(
+        &state,
+        StatusCode::Ok,
+        Some((
+            String::from("No appointment").into_bytes(),
+            mime::TEXT_PLAIN,
+        )),
+    );
+    (state, res)
+}
+
+fn router() -> Router {
+    build_simple_router(|route| {
+        route.get("/appointments").to(get_appointments);
+    })
+}
+
+fn main() {
+    let addr = "127.0.0.1:8080";
+    println!("Listening for requests at http://{}", addr);
+    gotham::start(addr, router());
+}
